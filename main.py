fae3740ef21383fff71946bae9a5351b078aee8b
Sajib Saha <sajibsaha859@gmail.com>
2023-03-31 15:46:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ model_engine = "text-davinci-003"
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
